87ce7537f1292e7bc07020c461a7d9f3a2a8b552
vibewill2 <vibewill@gmail.com>
2025-08-17 06:10:41 -0300

    novo mudanca

--- hypr/scripts/wallpaper_changer.sh
+++ hypr/scripts/wallpaper_changer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Diretório onde suas imagens estão. Mude para o caminho correto.
+# Diretório onde suas imagens estão
 WALLPAPERS_DIR="$HOME/Imagens/wallpapers/"
 
 # Verifique se o diretório existe
@@ -9,8 +9,18 @@ if [ ! -d "$WALLPAPERS_DIR" ]; then
     exit 1
 fi
 
-# Use o swww para pegar uma imagem aleatória e definir como papel de parede
-# O comando 'find' procura por arquivos, o '-type f' garante que são arquivos (não diretórios).
-# O 'shuf -n 1' pega uma linha aleatória (ou seja, um arquivo aleatório).
-# O 'xargs' passa essa linha como argumento para o swww.
-find "$WALLPAPERS_DIR" -type f | shuf -n 1 | xargs swww img
+# Escolhe uma imagem aleatória
+IMG=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
+
+# Mostra a imagem em uma caixinha de confirmação
+if yad --title="Definir Papel de Parede?" \
+       --image="$IMG" \
+       --button=Sim:0 \
+       --button=Não:1 \
+       --center \
+       --width=400 \
+       --height=400 \
+       --text="Deseja definir esta imagem como papel de parede?"; then
+    # Se clicar em Sim, aplica a imagem
+    swww img "$IMG"
+fi
